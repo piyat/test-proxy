825b469a6f9968252493086f20ef5708a56a5ebf
Piya Attapatto <piyatissaa@motabilityoperations.co.uk>
2021-12-09 22:36:11 +0000

    Initial commit

---
+++ server/configuration/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -f /etc/stunnel/pki/cert.pem ]; then
+    openssl genrsa -out /etc/stunnel/pki/key.pem 2048
+    openssl req -new -x509 -key /etc/stunnel/pki/key.pem -out /etc/stunnel/pki/cert.pem -days 1095 -subj "/C=XX/CN=stunnel-server"
+fi
+
+if [ -z CONNECT_PORT ]; then
+    sed -i -e "s/5001/${CONNECT_PORT}/" /etc/stunnel/config/config
+fi
+
+stunnel /etc/stunnel/config/config
