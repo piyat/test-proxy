9adc4d45ffe121313dc82da4d8a8e10af643bf88
Piya Attapatto <piyatissaa@motabilityoperations.co.uk>
2021-12-13 13:01:08 +0000

    mods

--- server/build/launch.sh
+++ server/build/launch.sh
@@ -5,8 +5,4 @@ if [ ! -f /etc/stunnel/pki/cert.pem ]; then
     openssl req -new -x509 -key /etc/stunnel/pki/key.pem -out /etc/stunnel/pki/cert.pem -days 1095 -subj "/C=XX/CN=stunnel-server"
 fi
 
-if [ -z CONNECT_PORT ]; then
-    sed -i -e "s/5001/${CONNECT_PORT}/" /etc/stunnel/config/stunnel.conf
-fi
-
 stunnel /etc/stunnel/config/stunnel.conf
